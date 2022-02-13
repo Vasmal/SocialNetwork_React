412a8c0a4c3ed5c15b2579c53053d7d52df28003
unknown <malaivasili@gmail.com>
2022-02-13 12:55:23 +0300

    Some changes in Style files

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,14 +4,34 @@ import classes from './Dialogs.module.css';
 function Dialogs(props) {
   return (
     <div className={classes.dialogs}>
-      <div className={classes.list}>
-        <div className={classes.dialog}>Sergey</div>
-        <div className={classes.dialog}>Sveta</div>
-        <div className={classes.dialog}>Victor</div>
-        <div className={classes.dialog}>Dima</div>
+      <div className={classes.title}>
+        Available dialogs
       </div>
-      <div className={classes.messages}>
-        <div className={classes.message}>ergerertg</div>
+      <div className={classes.content}>
+        <div className={classes.list}>
+          <div className={classes.dialog}>
+            <div className={classes.img}><img src="" alt="" /></div>
+            <div className={classes.name}>Sergey</div>
+          </div>
+          <div className={classes.dialog}>
+            <div className={classes.img}><img src="" alt="" /></div>
+            <div className={classes.name}>Sveta</div>
+          </div>
+          <div className={classes.dialog}>
+            <div className={classes.img}><img src="" alt="" /></div>
+            <div className={classes.name}>Victor</div>
+          </div>
+          <div className={classes.dialog}>
+            <div className={classes.img}><img src="" alt="" /></div>
+            <div className={classes.name}>Dima</div>
+          </div>
+        </div>
+
+        <div className={classes.messages}>
+          <div className={classes.message}>ergerertg ergerertg ergerertg ergerertg ergerertg ergerertg ergerertg ergerertg</div>
+          <div className={classes.message}>ergerertg</div>
+          <div className={classes.message}>ergerertg</div>
+        </div>
       </div>
     </div>
   );
