fa3c2423db56db38108aeee9138e31e2148960ad
unknown <malaivasili@gmail.com>
2022-05-27 09:11:19 +0300

    Messages-reducer refactoring, if else statement to switch case statement

--- src/redux/messages-reducer.js
+++ src/redux/messages-reducer.js
@@ -2,18 +2,21 @@ const ADD_MESSAGE = 'ADD-MESSAGE';
 const UPDATE_NEW_MESSAGE_TEXT = 'UPDATE-NEW-MESSAGE-TEXT';
 
 const messagesReducer = (state, action) => {
-  if (action.type === ADD_MESSAGE) {
-    let newMessage = {
-      id: 5,
-      message : state.newMessageText
-    }
-    state.dialogText.push(newMessage);
-    state.newMessageText = '';
-
-  } else if (action.type === UPDATE_NEW_MESSAGE_TEXT) {
+  switch (action.type) {
+    case ( ADD_MESSAGE):
+      let newMessage = {
+        id: 5,
+        message : state.newMessageText
+      }
+      state.dialogText.push(newMessage);
+      state.newMessageText = '';
+      return state;
+    case  ( UPDATE_NEW_MESSAGE_TEXT):
       state.newMessageText = action.newMessage;
+      return state;
+    default:
+      return state;
   }
-  return state;
 }
 
 export const addMessageActionCreate = () => {
