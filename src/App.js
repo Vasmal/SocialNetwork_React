38c7c15691cfe53ee2ff3caaccb4709eeaa218e0
unknown <malaivasili@gmail.com>
2022-06-14 00:57:32 +0300

    Created usersContainer component and Users component, added some styles

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import News from './components/News/News';
 import Settings from './components/Settings/Settings';
 import DialogsContainer from './components/Dialogs/DialogsСontainer';
 import FriendsOnlineContainer from './components/Friends/FriendsOnlineContainer';
-import Users from './components/Users/Users';
+import { UsersContainer } from './components/Users/UsersContainer';
 
 function App(props) {
   return (
@@ -28,7 +28,7 @@ function App(props) {
             <Route path='/dialogs/*' element={<DialogsContainer/>} />
             <Route path='/news' element={<News /> } />
             <Route path='/music' element={<Music />} />
-            <Route path='/users' element={<Users />} />
+            <Route path='/users' element={<UsersContainer />} />
             <Route path='/settings' element={<Settings />} />
           </Routes>
         </div>  
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,10 +1,38 @@
 import React from "react";
-import classes from './Users.module.css';
+import styles from './Users.module.css';
 
 const Users = (props) => {
   return (
-    <div className="">
-      Users
+    <div className={styles.users}>
+      <h1 className={styles.title}>Users</h1>
+      {
+        props.users.map( u => <div className={styles.user} key="u.id">
+          <div className={styles.user__logo}>
+            <span className={styles.user__img}><img src="" alt="" /></span>
+            { u.followed 
+                ? <button className={`${styles.btn} ${styles.btn_red}`} onClick={ () => {props.unfollow(u.id)} }>Unfollow</button> 
+                : <button className={`${styles.btn} ${styles.btn_blue}`} onClick={ () => {props.follow(u.id)} }>Follow</button>}
+          </div>
+          <div className={styles.user__info}>
+            <div className={styles.user__left}>
+              <p className={styles.user__name}>
+                {u.name}
+              </p>
+              <p className={styles.user__status}>
+                {u.status}
+              </p>
+            </div>
+            <div className={styles.user__location}>
+              <p className={styles.user__country}>
+                {u.location.country}
+              </p>
+              <p className={styles.user__city}>
+                {u.location.city}
+              </p>
+            </div>
+          </div>
+        </div>)
+      }
     </div>
   );
 }
